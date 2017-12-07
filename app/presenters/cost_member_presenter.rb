dc7eccb12cd2ed026cf41b2f7698ccfe43ffe2ad
takagotch <dyaccb@gmail.com>
2017-12-07 21:33:11 +0900

    app/presenters/[cost_member_presenter|model_presenter].rb && app/views/profit/cost_members/index.html.erb

--- app/presenters/cost_member_presenter.rb
+++ app/presenters/cost_member_presenter.rb
@@ -1,4 +1,9 @@
 class CostMemberPresenter < ModelPresenter
+delegate :suspended ?, to : object
+
+def suspended_mark
+	suspended? ? view_context.raw('&#x2611;') : ('&#x2611;')
+end
 end
 
 
--- app/presenters/model_presenter.rb
+++ app/presenters/model_presenter.rb
@@ -1,5 +1,6 @@
 class ModelPresenter
   attr_render :object, :view_context
+  delegate :raw, to: :view_context
 
   def initialize(object, view_context)
   @object = object
