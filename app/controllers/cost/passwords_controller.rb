7f2c63faaa911f9e2f0deb073dc5a12bb73a884f
takagotch <dyaccb@gmail.com>
2017-12-07 20:31:12 +0900

    app/[controllers|forms|assets]/[cost|stylesheets]/[\ |cost]/[passwords_controller|change_password_form|_colors|form].[rb|css.scss]

--- app/controllers/cost/passwords_controller.rb
+++ app/controllers/cost/passwords_controller.rb
@@ -1,13 +1,22 @@
 class Cost::PasswordsController < Cost::Base
 	#...
 def update
-
+	@change_password_form = Cost::ChangePasswordForm.new(staff_member_params)
+	@change_password_form.object = current_cost_mamber
+	if @change_password_form.save
+		flash.notice = 'Update Pass'
+		redirect_to :staff_account
+	else
+		flash.now.alert = 'invalidated u input'
+		render action: 'edit'
 end
 
 private
 def staff_member_params
+	params.require(:staff_change_password_force).permit(
+	:current_password, :new_password, :new_password_confirmation
+	)
 end
-
 end
 
 
--- app/forms/cost/change_password_form.rb
+++ app/forms/cost/change_password_form.rb
@@ -3,11 +3,20 @@ include ActiveModel::Model
 
 attr_accessor :object, :current_password, :new_password,
 	:new_password_confirmation
+validates :new_password, presence: true, confirmation: true
+
+validate do
+	unless Cost::Authenticator.new(object).authenticate(current_password)
+		errors.add(:current_password, :wrong)
+end
+
 
 def save
+	if vaid?
 	object.password = new_password
 	object.save!
-end
+	end
+	end
 
 end
 
