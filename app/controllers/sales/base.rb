c95062379524367b11a2d7ffe3196d1697bbb14d
takagotch <dyaccb@gmail.com>
2017-12-08 00:36:27 +0900

    app/controllers/sales/base.rb

---
+++ app/controllers/sales/base.rb
@@ -0,0 +1,21 @@
+class Sales::Base < ApplicatoinController
+	before_action :authorize
+
+	private
+	def current_customer
+	if session[:sales_id]
+		@current_customer ||=
+			Sales.find_by(id:session[:sales_id])
+	end
+	end
+
+	helper_method :current_sales
+
+	def authorize
+		unless current_sales
+			flash.alert = 'Login first'
+			redirect_to :sales_login
+		end
+	end
+end
+
