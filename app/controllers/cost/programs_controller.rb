46ca50f6b711efa83787aef23d941df65a9a7981
takagotch <dyaccb@gmail.com>
2017-12-08 14:41:58 +0900

    app/[assets/controllers|views]/[stylesheets|cost|\ ]/[programs|\ ]/[show|programs_contrler|divs_and_spans].[html.erb|rb|css.scss]

---
+++ app/controllers/cost/programs_controller.rb
@@ -0,0 +1,11 @@
+class Cost::ProgramsController < Cost::Base
+	def index
+		@programs = Program.order(application_start_time: :desc)
+			.page(params[:page])
+	end
+
+	def show
+		@program = Program.find(params[:id])
+	end
+end
+
