3a00f5aba026fea6f6e6afed54763ec54504f725
takagotch <dyaccb@gmail.com>
2017-12-08 09:04:54 +0900

    app/models/allowed_source.rb

--- app/models/allowed_source.rb
+++ app/models/allowed_source.rb
@@ -1,3 +1,12 @@
+
+class AllowedSource < ActiveRecord::Base
+	validates :octet1, octet2, octet3, octet4, presence: true,
+		numerivality: {only_integer: true, allow_blank: true,}
+	inclusion: {in: 0..255, allow_blank: true}
+	validates :octet4,
+		uniqueness: {scope: [:octet1, octet2, octet3], allow_blank: true}
+end
+
 validates :octet4,
 	uniqueness: {scope[:octet1, :octet2, :octet3], allow_blank: true}
 
