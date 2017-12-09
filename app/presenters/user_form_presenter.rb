d645a349fc8c1736cb03eeea09a7cf16304eaac0
takagotch <dyaccb@gmail.com>
2017-12-10 03:40:47 +0900

    class SaleFormPresenter < UserFromPresenter

---
+++ app/presenters/user_form_presenter.rb
@@ -0,0 +1,10 @@
+class UserFormPresenter < FormPresenter
+#def check_boxes
+#markup(:div, class: 'input-block') do |m|
+#m << checkbox(:suspended)
+#m << label(:suspended, 'Stop Accounts')
+#end
+#end
+
+end
+
