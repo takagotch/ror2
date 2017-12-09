9c0673369755b00433a7c9073eb8e53eae4ef330
takagotch <dyaccb@gmail.com>
2017-12-09 20:57:26 +0900

    label #class CostMemberFormPresenter

--- app/presenters/cost_member_presenter.rb
+++ app/presenters/cost_member_presenter.rb
@@ -4,6 +4,13 @@ delegate :suspended ?, to : object
 def suspended_mark
 	suspended? ? view_context.raw('&#x2611;') : ('&#x2611;')
 end
+
+def full_name_block(name1, name2, lable_text, options = {})
+	markup(:div) do |m|
+	m << decorated_label(name1, label_text, options)
+	m << text_field(name1, options)
+	m << text_field(name2, options)
+	end
 end
 
 
