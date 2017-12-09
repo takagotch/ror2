ef766a0f6b0fc0b5ad6116ef855d30a071b05ec4
takagotch <dyaccb@gmail.com>
2017-12-09 21:32:26 +0900

    EER

--- app/presenters/cost_member_form_presenter.rb
+++ app/presenters/cost_member_form_presenter.rb
@@ -7,11 +7,13 @@ class CostMemberFormPresenter < FormPresenter
 
 	def full_name_block(name1, name2, label_text, options ={})
 		markup(:div, class: 'input-block') do |m|
-			m << label(name1, lable_text,
-				   class: options[:required] ? 'required' : nil)
+			#m << label(name1, lable_text,
+			#	   class: options[:required] ? 'required' : nil)
 			m << decorated_label(name1, label_text, options)
 			m << text_field(name1, options)
 			m << text_field(name2, options)
+			m << error_messages_for(name1)
+			m << error_messages_for(name2)
 		end
 	end
 
