c9a6b414df7962aba806b8908652ac3d7f20e6c8
takagotch <dyaccb@gmail.com>
2017-12-10 23:50:45 +0900

    assign_attribute(params = {})

--- app/assets/javascripts/cost/sales_form.js.coffee
+++ app/assets/javascripts/cost/sales_form.js.coffee
@@ -9,4 +9,20 @@ $(document).on 'page:change', ->
     $('#enable-password-field').show()
     $('#form_sales_password').attr('disabled', 'disabled')
     $('label[for=form_sales_password]').remove('required')
+  toggle_home_address_fields()
+  toggle_work_address_fields()
+  $('input#form_inputs_home_address').on 'click', ->
+    toggle_home_address_fields()
+  $('input#form_inputs_work_address').on 'click', ->
+    toggle_work_address_fields()
+
+  toggle_home_address_fields = ->
+    checked = $('input#form_inputs_home_address').prop('checked')
+    $('fieldset#home-address-fields input').prop('disable', !checked)
+    $('fieldset#home-address-fields select').prop('disable', !checked)
+
+  toggle_work_address_fields = ->
+    checked = $('input#form_inputs_work_address').prop('checked')
+    $('fieldset#work-address-fields input').prop('disabled', !checked)
+    $('fieldset#work-address-fields select').prop('disabled', !checked)
 
--- app/forms/cost/sales_form.rb
+++ app/forms/cost/sales_form.rb
@@ -1,22 +1,36 @@
 class Cost::SalesForm
 	include ActiveModel::Model
 
-	attr_accessor :sales
-	delegate :persosted?, to: :sales
+	attr_accessor :sales, :inputs_home_address, :inputs_work_address
+	delegate :persosted?, :save, to: :sales
 
 	def initialize(sales = nil)
 		@sales = sales
 		@sales ||= Sales.new(gender: 'male')
+		self.inputs_home_address = @sales.home_address.persent?
+		self.inputs_work_address = @sales.work_address.persent?
 		@sales.build_home_address unless @sales.home_address
 		@sales.build_work_address unless @sales.work_address
 	end
 
 	def assign_attributes(params = {})
 	@params = params
+	self.inputs_home_address = params[:inputs_home_address] == '1'
+	self.inputs_work_address = params[:inputs_work_address] == '1'
 
-	sales.assign_attributes(sales_params = {})
+	sales.assign_attributes(sales_params)
+	if inputs_home_address
 	sales.home_address.assign_attributes(home_address_params)
+	else
+		#sales.home_address = nil
+		sales.home_address.mark_for_destruction
+	end
+	if inputs_work_address
 	sales.work_address.assign_attribures(work_address_params)
+	else
+		#sales.work_address = nil
+		sales.work_address.mark_for_destruction
+	end
 	end
 
 	def valid?
