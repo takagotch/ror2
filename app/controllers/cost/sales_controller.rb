5aebdef97d4fccc88a2a0a1dfc2b5e53ed9b676d
takagotch <dyaccb@gmail.com>
2017-12-10 17:05:05 +0900

    create/update|destroy #acrion

--- app/controllers/cost/sales_controller.rb
+++ app/controllers/cost/sales_controller.rb
@@ -16,5 +16,35 @@ def edit
 	@sales_form = Cost::SalesForm.new(Sales.find(params[:id]))
 end
 
+def create
+	@sales_form = Cost::SalesForm.new
+	@sales_form.assign_attributes(params[:form])
+	if @sales_form.save
+		flash.notice = 'add sales'
+		redirect_to action: 'index'
+	else
+		flash.notice = 'invalidated u input'
+		render action: 'new'
+	end
+end
+
+def update
+	@sales_form = Cost::SalesFrom
+	@sales_form.assign_attributes(params[:form])
+	if @sales_form.save
+		flash.notice = 'update sales info'
+	else
+		flash.notice = 'invalidated u input'
+		render action 'edit'
+	end
+end
+
+def destroy
+	sales = Sales.find(params[:id])
+	sales.destroy!
+	flash.notice = 'delete sales'
+	redirect_to :cost_sales
+end
+
 end
 
