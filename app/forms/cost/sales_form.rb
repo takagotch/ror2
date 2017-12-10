708b511e778df7d32aa6f0d3039f4de56d73d7cd
takagotch <dyaccb@gmail.com>
2017-12-10 16:55:20 +0900

    assign_attributes()

--- app/forms/cost/sales_form.rb
+++ app/forms/cost/sales_form.rb
@@ -10,5 +10,36 @@ class Cost::SalesForm
 		@sales.build_home_address unless @sales.home_address
 		@sales.build_work_address unless @sales.work_address
 	end
+
+	def assign_attributes(params = {})
+	@params = params
+
+	sales.assign_attributes(sales_params = {})
+	sales.home_address.assign_attributes(home_address_params)
+	sales.work_address.assign_attribures(work_address_params)
+	end
+
+	private
+	def sales_params
+		@params.require(:sales).permit(
+		:email, :password,
+		:family_name, :given_name, :family_name_kana, :given_name_kana,
+		:birthday, :gender
+		)
+	end
+
+	def home_address_params
+		@params.require(:home_address).permit(
+		:postal_code, :prefecture, :city, :address1, :address2,
+		)
+	end
+
+	def work_address_params
+		@params.require(:work_address).permit(
+		:postal_code, :prefecture, :city, :address1, :address2,
+		:company_name, :division_name
+		)
+	end
+
 end
 
