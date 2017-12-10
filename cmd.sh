64044e6c7295311f6132d2d9345b8ae1585c094e
takagotch <dyaccb@gmail.com>
2017-12-10 21:20:52 +0900

    cp

---
+++ cmd.sh
@@ -0,0 +1,9 @@
+cd app/views
+cp cost/sales/edit.html.erb sales/accounts
+cp cost/sales/_sales_fields.html.erb sales/accounts
+cp cost/sales/_form.html.erb sales/accounts
+cp cost/sales/_home_address_fields.html.erb sales/accounts
+cp cost/sales/_phone_fields.html.erb sales/accounts
+cp cost/sales/_work_address_fields.html.erb sales/accounts
+
+
