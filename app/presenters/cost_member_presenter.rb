535880ccaf4b914b3112d1ea38c14484b5fd9341
takagotch <dyaccb@gmail.com>
2017-12-14 00:08:26 +0900

    form_16

--- app/presenters/cost_member_presenter.rb
+++ app/presenters/cost_member_presenter.rb
@@ -1,6 +1,14 @@
 class CostMemberPresenter < ModelPresenter
 delegate :suspended ?, to : object
 
+def full_name
+	object.family_name + ' ' + object.given_name
+end
+
+def full_name_kana
+	object.family_name_kana + ' ' + object.given_name_kana
+end
+
 def suspended_mark
 	suspended? ? view_context.raw('&#x2611;') : ('&#x2611;')
 end
