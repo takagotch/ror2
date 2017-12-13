90aa3ceaa74cd7f0d251022c8ded086879ad23d7
takagotch <dyaccb@gmail.com>
2017-12-14 00:28:57 +0900

    sessions_13

--- app/controllers/profit/base.rb
+++ app/controllers/profit/base.rb
@@ -1,11 +1,41 @@
 class Profit::Base < ApplicationController
-	private
-	def current_administrator
-		if session[:administrator_id]
-			@current_addministrator ||=
-				Administrator.find_by(id: session[:administrator_id])
+before_action :authorize
+before_action :check_account
+before_action :check_timeout
+
+private
+
+
+def current_administrator
+	if session[:administrator_id]
+		@current_addministrator ||=
+			Administrator.find_by(id: session[:administrator_id])
+	end
+end
+  helper_method :current_administrator
+end
+
+def check_account
+	if current administrator && current_administrator.suspended?
+		session.delete(:administrator id)
+		flash.alert = 'invalided accoutns'
+		redirect_to :profit_root
+	end
+end
+
+TIMEOUT = 60.minutes
+
+def timeout
+	if current administrator
+		if session[:last access time] >= TIMEOUT.ago
+			session[] = Time.current
+		else
+			session.delete(:administrator id)
+			flash.alert = 'Session timeout'
+			redirect_to :profit_login
 		end
 	end
-	helper_method :current_administrator
+end
+
 end
 
