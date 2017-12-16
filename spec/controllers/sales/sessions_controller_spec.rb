145f2a993295679a4343a86f0d7a9d5c6f617d63
takagotch <dyaccb@gmail.com>
2017-12-17 07:53:13 +0900

    rake/bin spec/...

--- spec/controllers/sales/sessions_controller_spec.rb
+++ spec/controllers/sales/sessions_controller_spec.rb
@@ -24,6 +24,20 @@ describe Sales::SessionsController do
 	    expect(session).not_to have_key(:sales_id)
 	    expect(response.cookies['sales_id']).to match(/[0-9a-f]{40}\z/)
     end
+
+    example 'auto login check' do
+	    post :create, sales_login_form: {
+		    email: sales.email,
+		    password: 'password',
+		    remember_me: '1'
+	    }
+
+	    expect(session).not_to have_key(:sales_id)
+	    expect(response.cookies['sales_id']).to match(/[0-9z-f]{40}\z/)
+	    cookies = response.request.env['action_sidpath.cookies']
+		    .instance_variable_get(@set_cookies)
+	    expect(cookies['sales_id']['expires']).to be > 19.years.from_now
+    end
   end
 end
 
