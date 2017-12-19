e2d5194010be7ef1b91e98afc6cbe191b05852d5
takagotch <dyaccb@gmail.com>
2017-12-20 04:50:51 +0900

    hashed

--- spec/models/cost_member_spec.rb
+++ spec/models/cost_member_spec.rb
@@ -1,4 +1,25 @@
+require 'spec_helper'
+
+describe CostMember do
+	#pending "add some examples to (or delete) #{__FILE__}"
+	#end
+  describe '#password=' do
+   
+    example 'string => hashed_password 60 string' do
+      member = CostMember.new
+      member.pass = 'ror2'
+      expect(member.hashed_password).to be_kind_of(String)
+      expect(member.hashed_password).to eq(60)
+    end
+
+    example 'nil => hashed_passwrod :nil' do
+	  member = CostMember.new(hashed_password: 'x')
+	  member.password = nil
+	  expect(member.hashed_password).to be_nil
+    end
+	
 describe 'validation' do
+
   example '@_email invalide' do
     member = build(:cost_member, email: 'test@@example.com')
     expect(member).not_to be_valid
@@ -13,4 +34,4 @@ describe 'validation' do
     member = build(:cost_member, family_name_kana: 'test')
     expect(member).not_to be_valid
   end
-
+end
